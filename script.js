334168b9502aeb2b88f46a21a402554e5fa1dd12
Alisa Khaustova <alice.khaustova@gmail.com>
2020-03-23 15:00:27 +0200

    improve code add eventListener

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // https://api.telegram.org/bot1087301654:AAFZKpAZJul6BK-sl7-GU_NItuYkkDxFmTE/sendMessage?chat_id=-427190390&text=hi_i_am_a_bot;
 // https://api.telegram.org/bot1087301654:AAFZKpAZJul6BK-sl7-GU_NItuYkkDxFmTE/getUpdates;
-
-document.querySelector('button').onclick = function() {
+const token = '1087301654:AAFZKpAZJul6BK-sl7-GU_NItuYkkDxFmTE'
+const url = 'https://api.telegram.org/bot'+ token +'/sendMessage?chat_id=-427190390&text=';
+function pushMessage() {
     let message = document.querySelector('.message').value;
-    const token = '1087301654:AAFZKpAZJul6BK-sl7-GU_NItuYkkDxFmTE'
-    const url = 'https://api.telegram.org/bot'+ token +'/sendMessage?chat_id=-427190390&text=';
     let xhttp = new XMLHttpRequest();
     xhttp.open("GET", url + message, true);
     xhttp.send();
 
 }
+
+document.querySelector('button').addEventListener('click', pushMessage);
